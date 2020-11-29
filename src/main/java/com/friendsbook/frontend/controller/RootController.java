f3d01350b180ab2fc96ce9ea324e44e53f3219f1
Vaibhav Pratihar <vaibhavpratihar1771@gmail.com>
2020-11-29 15:16:49 +0530

    added root controller for wake up endpoint

---
+++ src/main/java/com/friendsbook/frontend/controller/RootController.java
@@ -0,0 +1,19 @@
+package com.friendsbook.frontend.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class RootController {
+
+	private Logger logger = LoggerFactory.getLogger(RootController.class);
+
+	@GetMapping("/wake-up")
+	public String wakeUp() {
+		logger.info("Wake up call recieved!!!");
+		return "I am up and running :-D";
+	}
+}
