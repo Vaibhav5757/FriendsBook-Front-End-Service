b136004b379bdcbe54a2e4a57fef227d78dc356c
Vaibhav Pratihar <vaibhavpratihar1771@gmail.com>
2020-11-29 20:32:19 +0530

    debugging

--- src/main/java/com/friendsbook/functional/FriendsBookFrontEndServiceApplication.java
+++ src/main/java/com/friendsbook/functional/FriendsBookFrontEndServiceApplication.java
@@ -2,10 +2,8 @@ package com.friendsbook.functional;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan
 public class FriendsBookFrontEndServiceApplication {
 
 	public static void main(String[] args) {
