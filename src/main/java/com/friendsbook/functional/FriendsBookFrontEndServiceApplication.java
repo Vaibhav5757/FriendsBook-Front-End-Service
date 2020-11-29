12db8e60e12851bfbf9b3805e8e891e5f74cadf0
Vaibhav Pratihar <vaibhavpratihar1771@gmail.com>
2020-11-29 20:50:13 +0530

    debugging

--- src/main/java/com/friendsbook/functional/FriendsBookFrontEndServiceApplication.java
+++ src/main/java/com/friendsbook/functional/FriendsBookFrontEndServiceApplication.java
@@ -3,9 +3,7 @@ package com.friendsbook.functional;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.friendsbook.frontend.security.SecurityCenter;
-
-@SpringBootApplication(exclude = SecurityCenter.class)
+@SpringBootApplication
 public class FriendsBookFrontEndServiceApplication {
 
 	public static void main(String[] args) {
