1ab2819dde49a4e8102710a1e1094bad29218cf9
Vaibhav Pratihar <vaibhavpratihar1771@gmail.com>
2020-11-29 15:11:49 +0530

    decoupled Eureka Server and Front-End service

--- src/main/java/com/friendsbook/functional/FriendsBookFrontEndServiceApplication.java
+++ src/main/java/com/friendsbook/functional/FriendsBookFrontEndServiceApplication.java
@@ -2,10 +2,8 @@ package com.friendsbook.functional;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.ComponentScan;
 
-@EnableEurekaServer
 @SpringBootApplication
 @ComponentScan
 public class FriendsBookFrontEndServiceApplication {
