4b66820aed8f969f9323bf65193eddc64f14184c
Vaibhav Pratihar <vaibhavpratihar1771@gmail.com>
2020-11-29 20:41:58 +0530

    debugging

--- src/main/java/com/friendsbook/frontend/security/SecurityCenter.java
+++ src/main/java/com/friendsbook/frontend/security/SecurityCenter.java
@@ -17,22 +17,25 @@ public class SecurityCenter extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		
-		http
-			.formLogin().disable()
-			.logout().disable();
-		
-		http
-			.httpBasic().disable()
-			.csrf().disable()// disable cross site request forgery
-			.cors().disable()// disable cors
-			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-			.and()
-			.authorizeRequests()// authorize requests
-			.antMatchers("/user/sign-up").permitAll()// allow these url(s) to be public
-			.antMatchers("/user/*").hasRole("USER")
-			.antMatchers("/admin/*").hasRole("ADMIN")
-			.and()
-			.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+		http.
+			authorizeRequests()
+			.anyRequest()
+			.permitAll();
+//		http
+//			.formLogin().disable()
+//			.logout().disable();
+//		
+//		http
+//			.httpBasic().disable()
+//			.csrf().disable()// disable cross site request forgery
+//			.cors().disable()// disable cors
+//			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//			.and()
+//			.authorizeRequests()// authorize requests
+//			.antMatchers("/user/sign-up").permitAll()// allow these url(s) to be public
+//			.antMatchers("/user/*").hasRole("USER")
+//			.antMatchers("/admin/*").hasRole("ADMIN")
+//			.and()
+//			.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 	}
 }
