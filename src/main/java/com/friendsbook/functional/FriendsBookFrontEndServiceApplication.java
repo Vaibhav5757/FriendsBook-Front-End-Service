87d431ecabd754ee5229ebefe13ada7039896585
Vaibhav Pratihar <vaibhavpratihar1771@gmail.com>
2020-12-02 23:03:07 +0530

    removed Eureka Client annotation

--- src/main/java/com/friendsbook/functional/FriendsBookFrontEndServiceApplication.java
+++ src/main/java/com/friendsbook/functional/FriendsBookFrontEndServiceApplication.java
@@ -3,11 +3,9 @@ package com.friendsbook.functional;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EnableEurekaClient
 @ComponentScan(basePackages = {"com.friendsbook"})
 @EntityScan(basePackages="com.friendsbook")
 public class FriendsBookFrontEndServiceApplication {
