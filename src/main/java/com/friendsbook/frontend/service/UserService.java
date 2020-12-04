108fe46da02760b2dc917a498f91850fc4dce653
Vaibhav Pratihar <vaibhavpratihar1771@gmail.com>
2020-12-04 22:32:00 +0530

    removed hard coded urls for different microservice

--- src/main/java/com/friendsbook/frontend/service/UserService.java
+++ src/main/java/com/friendsbook/frontend/service/UserService.java
@@ -56,7 +56,7 @@ public class UserService {
 	public ResponseEntity<ApiResponse> createUser(User obj) {
 		HttpEntity<User> requestEntity = new HttpEntity<User>(obj, headers);
 		ResponseEntity<String> response =  this.userSvcHttp.exchange(
-						"https://friendsbook-user-service.herokuapp.com/user/sign-up",
+						"https://USER-MICROSERVICE/user/sign-up",
 						HttpMethod.POST,
 						requestEntity,
 						String.class);
@@ -68,7 +68,7 @@ public class UserService {
 	public ResponseEntity<ApiResponse> login(LoginBody obj){
 		HttpEntity<LoginBody> requestEntity = new HttpEntity<LoginBody>(obj, headers);
 		ResponseEntity<User> response =  this.userSvcHttp.exchange(
-				"https://friendsbook-user-service.herokuapp.com/user/log-in",
+				"https://USER-MICROSERVICE/user/log-in",
 				HttpMethod.POST,
 				requestEntity,
 				User.class);
