8404afddbd1697a96509ecff71aea38bd37ffc46
Vaibhav Pratihar <vaibhavpratihar1771@gmail.com>
2020-12-04 23:10:09 +0530

    added authentication header

--- src/main/java/com/friendsbook/frontend/util/ApplicationInitializer.java
+++ src/main/java/com/friendsbook/frontend/util/ApplicationInitializer.java
@@ -1,11 +1,16 @@
 package com.friendsbook.frontend.util;
 
+import java.util.Base64;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -15,16 +20,28 @@ public class ApplicationInitializer implements CommandLineRunner {
 	
 	@Value("${userservice.api.url}")
 	private String userMicroserviceUrl;
+	@Value("${userservice.api.username}")
+	private String userServiceBasicAuthUsername;
+	@Value("${userservice.api.password}")
+	private String userServiceBasicAuthUserPassword;
 	
 	@Autowired
 	private RestTemplate http;
 	
 	private Logger logger = LoggerFactory.getLogger(ApplicationInitializer.class);
+	
+	private HttpHeaders headers;
 
 	@Override
 	public void run(String... args) throws Exception {
 		
-		ResponseEntity<String> response = this.http.exchange(userMicroserviceUrl + "/wake-up", HttpMethod.GET, null, String.class);
+		String userSeviceAuthStr = userServiceBasicAuthUsername + ":" + userServiceBasicAuthUserPassword;
+		String userSeviBase64Creds = Base64.getEncoder().encodeToString(userSeviceAuthStr.getBytes());
+		headers = new HttpHeaders();
+		headers.set("Authorization","Basic " + userSeviBase64Creds);
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		
+		ResponseEntity<String> response = this.http.exchange(userMicroserviceUrl + "/wake-up", HttpMethod.GET, new HttpEntity<Object>(null, headers), String.class);
 		if(response.getBody().equals("Yo, I Woke up!!!")) {
 			logger.info("User Microservice woke up");
 		}else {
