6d8ce24fe7054bf6d05f30bdf95cc74a17e97bab
Vaibhav Pratihar <vaibhavpratihar1771@gmail.com>
2020-12-04 23:29:14 +0530

    debugging

--- src/main/java/com/friendsbook/functional/FriendsBookFrontEndServiceApplication.java
+++ src/main/java/com/friendsbook/functional/FriendsBookFrontEndServiceApplication.java
@@ -3,6 +3,7 @@ package com.friendsbook.functional;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
@@ -17,6 +18,7 @@ public class FriendsBookFrontEndServiceApplication {
 	}
 	
 	@Bean
+	@LoadBalanced
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
 	}
