e2e2513ecd0dafe4cd68c495bcd6ef832536581a
Vaibhav Pratihar <vaibhavpratihar1771@gmail.com>
2020-12-10 23:47:24 +0530

    removed feign client

--- src/main/java/com/friendsbook/functional/FriendsBookFrontEndServiceApplication.java
+++ src/main/java/com/friendsbook/functional/FriendsBookFrontEndServiceApplication.java
@@ -3,13 +3,11 @@ package com.friendsbook.functional;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.friendsbook"})
 @EntityScan(basePackages="com.friendsbook")
-@EnableFeignClients
 public class FriendsBookFrontEndServiceApplication {
 
 	public static void main(String[] args) {
