9e964958037172d95235c2276fbabce5369d74ae
Vaibhav Pratihar <vaibhavpratihar1771@gmail.com>
2020-12-20 00:11:19 +0530

    added authentication properties for user service

--- src/main/java/com/friendsbook/frontend/controller/UserController.java
+++ src/main/java/com/friendsbook/frontend/controller/UserController.java
@@ -1,8 +1,17 @@
 package com.friendsbook.frontend.controller;
 
 import java.net.SocketTimeoutException;
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Map;
 
+import javax.annotation.PostConstruct;
+
+import org.bouncycastle.util.encoders.Base64;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,9 +28,27 @@ public class UserController {
 	@Autowired
 	private UserServiceClient client;
 	
+	Map<String, String> userServiceHeaders = new HashMap<String, String>();
+	
+	@Value("${userservice.api.username}")
+	private String userServiceUsername;
+	@Value("${userservice.api.password}")
+	private String userServicePassword;
+	
+	private Logger logger = LoggerFactory.getLogger(UserController.class);
+	
+	@PostConstruct
+	public void addHeaderProperties() {
+		String auth = userServiceUsername + ":" + userServicePassword;
+		byte[] encodedAuth = Base64.encode(auth.getBytes(Charset.forName("US-ASCII")));
+		String authHeader = "Basic " + new String(encodedAuth);
+		userServiceHeaders.put("Authorization", authHeader);
+		logger.info(authHeader);
+	}
+	
 	@PostMapping("/sign-up")
 	public String createUser(@RequestBody User obj) throws SocketTimeoutException {
-		return this.client.createUser(obj);
+		return this.client.createUser(userServiceHeaders,obj);
 	}
 	
 	@GetMapping("/only-user")
--- src/main/java/com/friendsbook/frontend/util/UserServiceClientFallback.java
+++ src/main/java/com/friendsbook/frontend/util/UserServiceClientFallback.java
@@ -1,12 +1,14 @@
 package com.friendsbook.frontend.util;
 
+import java.util.Map;
+
 import com.friendsbook.frontend.model.User;
 import com.friendsbook.frontend.service.UserServiceClient;
 
 public class UserServiceClientFallback implements UserServiceClient {
 
 	@Override
-	public String createUser(User obj) {
+	public String createUser(Map<String, String> headers,User obj) {
 		return "Unable to create User";
 	}
 
