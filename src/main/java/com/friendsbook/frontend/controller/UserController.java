7153e62bb9326c0485f0f26971cadf15498fab63
Vaibhav Pratihar <vaibhavpratihar1771@gmail.com>
2020-12-20 00:51:50 +0530

    trying with resttemplate

--- src/main/java/com/friendsbook/frontend/controller/UserController.java
+++ src/main/java/com/friendsbook/frontend/controller/UserController.java
@@ -12,11 +12,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.friendsbook.frontend.model.User;
 import com.friendsbook.frontend.service.UserServiceClient;
@@ -30,12 +35,17 @@ public class UserController {
 	
 	Map<String, String> userServiceHeaders = new HashMap<String, String>();
 	
+	private HttpHeaders headers;
+	
 	@Value("${userservice.api.username}")
 	private String userServiceUsername;
 	@Value("${userservice.api.password}")
 	private String userServicePassword;
 	
 	private Logger logger = LoggerFactory.getLogger(UserController.class);
+	
+	@Autowired
+	private RestTemplate userServiceRestTemplate;
 	
 	@PostConstruct
 	public void addHeaderProperties() {
@@ -45,11 +55,21 @@ public class UserController {
 		String authHeader = "Basic " + auth;
 		userServiceHeaders.put("Authorization", authHeader);
 		logger.info(authHeader);
+		
+		headers = new HttpHeaders();
+		headers.setBasicAuth(userServiceUsername, userServicePassword);
+		
 	}
 	
 	@PostMapping("/sign-up")
-	public String createUser(@RequestBody User obj) throws SocketTimeoutException {
-		return this.client.createUser(userServiceHeaders,obj);
+	public ResponseEntity<String> createUser(@RequestBody User obj) throws SocketTimeoutException {
+		return this.userServiceRestTemplate.exchange(
+				"https://User-Microservice//user/sign-up",
+				HttpMethod.POST,
+				new HttpEntity<User>(obj, headers),
+				String.class
+				);
+//		return this.client.createUser(userServiceHeaders,obj);
 	}
 	
 	@GetMapping("/only-user")
